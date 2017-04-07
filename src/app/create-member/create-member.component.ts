ca8e854bfd073e63403a9d853c4aaa62774a860b
Sam Kirsch <samdkirsch@gmail.com>
2017-04-07 11:04:41 -0700

    fill out the create member form component

--- src/app/create-member/create-member.component.ts
+++ src/app/create-member/create-member.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MemberService } from "../member.service";
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-create-member',
   templateUrl: './create-member.component.html',
-  styleUrls: ['./create-member.component.css']
+  styleUrls: ['./create-member.component.css'],
+  providers: [MemberService]
 })
 export class CreateMemberComponent implements OnInit {
 
-  constructor() { }
+  constructor(private memberService: MemberService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  saveMember(name: string, playerClass: string, characterName: string, race: string, backstory: string) {
+    var newMember = new Member(name, playerClass, characterName, race, backstory);
+    this.memberService.addMember(newMember);
+    this.router.navigate(['members']);
   }
 
 }
